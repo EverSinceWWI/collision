eb6f5117b4545629d39c9e68ab074d9f96cdb2b7
EverSinceWWI <32314108+EverSinceWWI@users.noreply.github.com>
2019-04-06 01:27:47 +0200

    improved collision & UI

--- script.js
+++ script.js
@@ -1,107 +1,98 @@
-// Wait for the page to be ready
-window.addEventListener("load", function(e) {
+window.onload = function() {
+      var webcam = document.getElementById('webcam');
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d')
 
-  console.log("Page loaded!");
+      if (navigator.mediaDevices.getUserMedia) {
+        var successCallback = function(stream) {
+          webcam.srcObject = stream;
+        };
+        var errorCallback = function(error) {
+          console.log(error);
+        };
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: { facingMode: { ideal: 'environment' } } // prefer rear-facing camera
+        }).then(successCallback, errorCallback);
+      };
 
-  // Store the color we will be tracking (selectable by clicking on the webcam feed)
-  var color = {r: 255, g: 0, b: 0};
-  var color2 = {r: 0, g: 0, b: 255};
-
-
-
-  // Grab reference to the tags we will be using
-  var slider = document.getElementById("tolerance");
-  var canvas  = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
-  var webcam = document.getElementById('webcam');
-  var swatch = document.getElementById("color");
-
-  if (navigator.mediaDevices.getUserMedia) {
-    var successCallback = function(stream) {
-      webcam.srcObject = stream;
-    };
-    var errorCallback = function(error) {
-      console.log(error);
-    };
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: { facingMode: { ideal: 'environment' } } // prefer rear-facing camera
-    }).then(successCallback, errorCallback);
+tracking.ColorTracker.registerColor('red', function(r, g, b) {
+  if (r > 120 && g < 60 && b < 60) {
+    return true;
   }
-
-  // Register our custom color tracking function
-  tracking.ColorTracker.registerColor('dynamic', function(r, g, b) {
-    return getColorDistance(color, {r: r, g: g, b: b}) < slider.value
-  });
-
-  // Create the color tracking object
-  var tracker = new tracking.ColorTracker("dynamic");
-var colors = new tracking.ColorTracker(['red', 'blue', 'green']);
-  // Add callback for the "track" event
-  tracker.on('track', function(e) {
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (e.data.length !== 0) {
-
-      e.data.forEach(function(rect) {
-        // console.log(rect);
-        drawRect(rect, context, color);
-      });
-
-    }
-
-  });
-
-  // Start tracking
-  tracking.track(webcam, tracker, { camera: true } );
-
-  // Add listener for the click event on the video
-  webcam.addEventListener("click", function (e) {
-
-    // Grab color from the \ feed where the click occured
-    var c = getColorAt(webcam, e.offsetX, e.offsetY);
-
-    // Update target color
-    color.r = c.r;
-    color.g = c.g;
-    color.b = c.b;
-
-    // Update the div's background so we can see which color was selected
-    swatch.style.backgroundColor = "rgb(" + c.r + ", " + c.g + ", " + c.b + ")";
-
-  });
-
+  return false;
+});
+tracking.ColorTracker.registerColor('blue', function(r, g, b) {
+  if (r < 50 && g < 50 && b > 50) {
+    return true;
+  }
+  return false;
 });
 
-// Calculates the Euclidian distance between the target color and the actual color
-function getColorDistance(target, actual) {
-  return Math.sqrt(
-    (target.r - actual.r) * (target.r - actual.r) +
-    (target.g - actual.g) * (target.g - actual.g) +
-    (target.b - actual.b) * (target.b - actual.b)
-  );
+var colors = new tracking.ColorTracker(['red','blue']);
+// colors.setMinDimension(50);
+// colors.setMinGroupSize(50);
+  // var tol = 5;
+colors.on('track', function(event) {
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  if (event.data.length === 0) {
+    // No colors were detected in this frame.
+  } else {
+    event.data.forEach(function(rect)
+    {
+      if (rect.colors === 'custom') {
+        rect.color = colors.customColor;
+      }
+      context.strokeStyle = rect.colors;
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      context.fillText('+',rect.x + rect.width/2, rect.y + rect.height/2);
+    });
+  }
+  if(event.data.length > 0) {
+    document.getElementById('car1').innerHTML=event.data[0].color + " car"
+
+    if(event.data.length>1 && (event.data[0].color !== event.data[1].color)) {
+    document.getElementById('car2').innerHTML=event.data[1].color + " car";
+
+    var car1 = event.data[0];
+    var car2 = event.data[1];
+
+    var x1 = event.data[0].x;
+    var x2 = event.data[1].x;
+
+    var y1 = event.data[0].y;
+    var y2 = event.data[1].y;
+
+    var w1 = event.data[0].width;
+    var w2 = event.data[1].width;
+
+    var h1 = event.data[0].height;
+    var h2 = event.data[1].height;
+
+    // if ((
+    //   Math.abs((event.data[0].x - event.data[1].x))<=tol ||Math.abs((event.data[0].y - event.data[1].y))<=tol)&&
+    //   (
+    //     event.data[0].color !== event.data[1].color
+    //   ))
+    if (((x1 <= x2 && x1+w1 >= x2) &&
+		(y1 <= y2 && y1+h1 >= y2))
+    && event.data[0].color !== event.data[1].color)
+     {
+       document.getElementById("collision").style.background='red'
+       document.getElementById("collision").innerHTML="COLLISION DETECTED";
+     }
+     else{
+       document.getElementById("collision").innerHTML="";
+       document.getElementById("collision").style.background='none';
+     }
+  }
 }
+});
 
-// Returns the color at the specified x/y location in the webcam video feed
-function getColorAt(webcam, x, y) {
-
-  // To be able to access pixel data from the webcam feed, we must first draw the current frame in
-  // a temporary canvas.
-  var canvas = document.createElement('canvas');
-  var context = canvas.getContext('2d');
-  canvas.width = webcam.width;
-  canvas.height = webcam.height;
-  context.drawImage(webcam, 0, 0, webcam.width, webcam.height);
-
-  // Then we grab the pixel information from the temp canvas and return it as an object
-  var pixel = context.getImageData(x, y, 1, 1).data;
-  return {r: pixel[0], g: pixel[1], b: pixel[2]};
-
-}
-
-// Draw a colored rectangle on the canvas
-function drawRect(rect, context, color) {
-  context.strokeStyle = "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
-  context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-}
+tracking.track('#webcam', colors);
+};
